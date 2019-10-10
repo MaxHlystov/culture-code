647a99bfd3794727328ecee5e97804a4922dd892
MaxHlystov <DhwU2qXsZ7M81Pm3ws>
2019-10-10 21:21:13 +0300

    Added movie's type.

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/movies/MovieType.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/movies/MovieType.kt
@@ -3,5 +3,6 @@ package ru.fmtk.khlystov.culture_code.model.movies
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
+// Movie, Series
 @Document(collection = "Movies_Types")
 data class MovieType(@Id val id: String?, val name: String)
