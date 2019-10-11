b5a5aec7d9cb48cecd868339e477bb9ad0a4e581
MaxHlystov <maxvls@gmail.com>
2019-10-11 23:24:40 +0300

    Moved itemRating to appropriate package.

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/ratings/ItemRating.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/ratings/ItemRating.kt
@@ -3,7 +3,6 @@ package ru.fmtk.khlystov.culture_code.model.ratings
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
-import ru.fmtk.khlystov.culture_code.model.ItemType
 import ru.fmtk.khlystov.culture_code.model.User
 
 @Document(collection = "Ratings_UserItemRating")
--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/ratings/ItemType.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/ratings/ItemType.kt
@@ -1,4 +1,4 @@
-package ru.fmtk.khlystov.culture_code.model
+package ru.fmtk.khlystov.culture_code.model.ratings
 
 enum class ItemType {
     BOOK, MOVIE, MUSIC, GAME
