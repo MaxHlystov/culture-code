3f8dc0d4f4d2b08ae544df9679e9f19e17fc4f98
MaxHlystov <maxvls@gmail.com>
2019-10-11 23:03:31 +0300

    Added user.

---
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/User.kt
@@ -0,0 +1,45 @@
+package ru.fmtk.khlystov.culture_code.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
+import kotlin.streams.toList
+
+@Document(collection = "Main_Users")
+data class User(@Id var id: String?,
+                val name: String,
+                val email: String = "",
+                val password: String = "",
+                val accountNonExpired: Boolean = true,
+                val accountNonLocked: Boolean = true,
+                val credentialsNonExpired: Boolean = true,
+                val enabled: Boolean = true,
+                val roles: Set<String> = HashSet()) {
+
+    constructor(name: String) : this(null, name)
+    constructor(user: User) : this(user.id,
+            user.name,
+            user.email,
+            user.password,
+            user.accountNonExpired,
+            user.accountNonLocked,
+            user.credentialsNonExpired,
+            user.enabled,
+            user.roles.asSequence().toHashSet())
+
+
+    fun newWithRoles(roles: Collection<String>) = User(
+            id,
+            name,
+            email,
+            password,
+            accountNonExpired,
+            accountNonLocked,
+            credentialsNonExpired,
+            enabled,
+            this.roles + roles)
+
+    override fun toString(): String {
+        return "User(id=$id, name='$name', password='$password')"
+    }
+}
