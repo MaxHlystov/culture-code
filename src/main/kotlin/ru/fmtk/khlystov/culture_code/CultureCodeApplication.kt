7a79fbbbed47e1dfc2ebffe6dabd57ad53312d91
MaxHlystov <maxvls@gmail.com>
2019-10-11 23:08:00 +0300

    Added user.

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/CultureCodeApplication.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/CultureCodeApplication.kt
@@ -1,9 +1,11 @@
 package ru.fmtk.khlystov.culture_code
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties
 class CultureCodeApplication
 
 fun main(args: Array<String>) {
