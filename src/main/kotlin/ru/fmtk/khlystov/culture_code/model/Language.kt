da60d033a1a2cc3ab345f184ba0104fa653d545b
MaxHlystov <maxvls@gmail.com>
2019-10-13 15:02:46 +0300

    Simplify move model.

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/Language.kt
+++
@@ -1,7 +0,0 @@
-package ru.fmtk.khlystov.culture_code.model
-
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
-
-@Document(collection = "Main_Languages")
-data class Language(@Id val id: String?, val name: String)
--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/movies/Movie.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/movies/Movie.kt
@@ -5,14 +5,13 @@ import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.format.annotation.DateTimeFormat
 import ru.fmtk.khlystov.culture_code.model.Country
-import ru.fmtk.khlystov.culture_code.model.Language
 import ru.fmtk.khlystov.culture_code.model.Person
 import java.time.LocalDateTime
 
 @Document(collection = "Movies_Movies")
 data class Movie(@Id val id: String,
                  val Title: String,
-                 @DBRef val type: MovieType,
+                 val type: MovieType,
                  val year: Short,
                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                  val released: LocalDateTime,
@@ -20,8 +19,6 @@ data class Movie(@Id val id: String,
                  @DBRef val genres: Set<MovieGenre> = HashSet(),
                  @DBRef val director: Person,
                  @DBRef val actors: Set<Person> = HashSet(),
-                 @DBRef val translators: Set<Person> = HashSet(),
-                 @DBRef val languages: Set<Language> = HashSet(),
                  @DBRef val country: Country,
                  val website: String,
                  val posterUrl: String,
