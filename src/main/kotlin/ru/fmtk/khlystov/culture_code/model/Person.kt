c8733ca1d9236c7c703806e296430da6447c6d35
MaxHlystov <maxvls@gmail.com>
2019-10-13 15:01:38 +0300

    Added birthday.

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/Person.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/Person.kt
@@ -2,8 +2,12 @@ package ru.fmtk.khlystov.culture_code.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDateTime
 
 @Document(collection = "Main_Persons")
 data class Person(@Id val id: String?,
                   val fullName: String,
-                  val shortBio: String)
+                  val shortBio: String,
+                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                  val birthday: LocalDateTime)
