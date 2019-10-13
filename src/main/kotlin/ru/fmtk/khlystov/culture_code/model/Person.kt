32876ccdd3e58555b22aa87e2d7c2496114bf1cf
MaxHlystov <maxvls@gmail.com>
2019-10-13 16:41:55 +0300

    Changed date-time to the date format

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/Person.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/Person.kt
@@ -4,11 +4,11 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.format.annotation.DateTimeFormat
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 @Document(collection = "Main_Persons")
 data class Person(@Id val id: String?,
                   @Indexed val fullName: String,
                   val shortBio: String,
-                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-                  val birthday: LocalDateTime)
+                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                  val birthday: LocalDate)
