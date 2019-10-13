82d79c4d2b283936f76f0fcf4600d5cb6f63d49b
MaxHlystov <maxvls@gmail.com>
2019-10-13 15:00:57 +0300

    Made books' genres more useful

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/books/BookGenre.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/books/BookGenre.kt
@@ -4,4 +4,12 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "Books_Genres")
-data class BookGenre (@Id val id: String?, val name: String)
+data class BookGenre(@Id val id: String?,
+                     val name: String,
+                     val group: String = "") {
+    override fun toString(): String = if (group.isEmpty()) {
+        name
+    } else {
+        "$group: $name"
+    }
+}
