f72f51900f5fea77da4a4a6f5464e02e01c98afa
MaxHlystov <maxvls@gmail.com>
2019-10-13 18:23:55 +0300

    Added books initialize. Simplified code.

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/changelogs/InitMongoDBDataChangeLog.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/changelogs/InitMongoDBDataChangeLog.kt
@@ -4,6 +4,8 @@ import com.github.cloudyrock.mongock.ChangeLog
 import com.github.cloudyrock.mongock.ChangeSet
 import org.springframework.data.mongodb.core.MongoTemplate
 import ru.fmtk.khlystov.culture_code.model.Country
+import ru.fmtk.khlystov.culture_code.model.Person
+import ru.fmtk.khlystov.culture_code.model.books.Book
 import ru.fmtk.khlystov.culture_code.model.books.BookGenre
 import ru.fmtk.khlystov.culture_code.model.movies.MovieGenre
 import ru.fmtk.khlystov.culture_code.model.music.MusicGenre
@@ -13,33 +15,63 @@ class InitMongoDBDataChangeLog {
 
     @ChangeSet(order = "000", id = "initBookGenres", author = "khlystov", runAlways = false)
     fun initBookGenres(template: MongoTemplate) {
-        getListsOfBookGenres().forEach { (group, sequence) ->
-            sequence.forEach { genre -> template.save(BookGenre(null, genre, group)) }
+        template.insertAllWithTransform(getListsOfBookGenres().asSequence()) { (group, sequence) ->
+            sequence.map { genre -> BookGenre(null, genre, group) }
         }
     }
 
     @ChangeSet(order = "001", id = "initCountries", author = "khlystov", runAlways = false)
     fun initCountries(template: MongoTemplate) {
-        getListOfCountries().forEach { (name, alpha3, iso) ->
-            template.save(Country(null, name, alpha3, iso.toShort()))
+        template.insertAllWithTransform(getListOfCountries()) { (name, alpha3, iso) ->
+            Country(null, name, alpha3, iso.toShort())
         }
     }
 
     @ChangeSet(order = "002", id = "initMovieGenres", author = "khlystov", runAlways = false)
     fun initMovieGenres(template: MongoTemplate) {
-        getListsOfMovieGenres().forEach { genre -> template.save(MovieGenre(null, genre)) }
+        template.insertAllWithTransform(getListsOfMovieGenres()) { genre ->
+            MovieGenre(null, genre)
+        }
     }
 
     @ChangeSet(order = "003", id = "initMusicGenres", author = "khlystov", runAlways = false)
     fun initMusicGenres(template: MongoTemplate) {
-        getListOfMusicGenres().forEach { genre -> template.save(MusicGenre(null, genre)) }
+        template.insertAllWithTransform(getListOfMusicGenres()) { genre ->
+            template.save(MusicGenre(null, genre))
+        }
     }
 
-//    @ChangeSet(order = "002", id = "initBooks", author = "khlystov", runAlways = false)
-//    fun initBooks(template: MongoTemplate) {
-//        template.save<Book>(Book(null,
-//                "Пушкин А.,Лерма",
-//                "Евгений Онегин.Герой нашего времени","978-5-8475-1147-6",2019,"МИРОВАЯ ХУДОЖЕСТВЕННАЯ ЛИТЕРАТУРА"
-//        ))
-//    }
+    @ChangeSet(order = "004", id = "initBooks", author = "khlystov", runAlways = false)
+    fun initBooks(template: MongoTemplate) {
+        val genres = template.findAll(BookGenre::class.java).groupBy(BookGenre::name)
+        val savedAuthors = template.insertAllWithTransform(ListOfBooks.authors.asSequence()) { (_, author) -> author }
+                .groupBy(Person::fullName)
+        template.insertAllWithTransform(ListOfBooks.books.asSequence()) { book ->
+            book.copyWithSubstitute(genres, savedAuthors)
+        }
+    }
+}
+
+private fun <T, R> MongoTemplate.insertAllWithTransform(sequence: Sequence<T>,
+                                                        transform: (value: T) -> R): Collection<R> =
+        this.insertAll(sequence.map(transform).toList())
+
+private fun Book.copyWithSubstitute(genres: Map<String, List<BookGenre>>,
+                                    persons: Map<String, List<Person>>): Book {
+    return Book(this.id,
+            this.title,
+            this.year,
+            this.genres.map { genre -> genres.getFirstOrDefault(genre.name, genre) }.toSet(),
+            this.writers.map { person -> persons.getFirstOrDefault(person.fullName, person) }.toSet(),
+            this.posterUrl,
+            this.description
+    )
+}
+
+private fun <ID, T> Map<ID, List<T>>.getFirstOrDefault(id: ID, default: T): T {
+    val items: List<T>? = this[id]
+    if (items == null || items.size == 0) {
+        return default
+    }
+    return items[0]
 }
