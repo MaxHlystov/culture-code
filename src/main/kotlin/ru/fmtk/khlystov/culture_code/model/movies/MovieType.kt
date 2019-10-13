6ec587342dd9d8563818fb6d98f53e65e8da5815
MaxHlystov <maxvls@gmail.com>
2019-10-13 15:02:04 +0300

    Made movie type enum.

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/model/movies/MovieType.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/model/movies/MovieType.kt
@@ -3,6 +3,5 @@ package ru.fmtk.khlystov.culture_code.model.movies
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-// Movie, Series
-@Document(collection = "Movies_Types")
-data class MovieType(@Id val id: String?, val name: String)
+
+enum class MovieType { MOVIE, SERIES }
