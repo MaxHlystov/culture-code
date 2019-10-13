1d4c49a60782ca0ec71f0a4aa6ccfac0e71b5212
MaxHlystov <maxvls@gmail.com>
2019-10-13 15:35:08 +0300

    Added music's genres initialization.

--- src/main/kotlin/ru/fmtk/khlystov/culture_code/changelogs/InitMongoDBDataChangeLog.kt
+++ src/main/kotlin/ru/fmtk/khlystov/culture_code/changelogs/InitMongoDBDataChangeLog.kt
@@ -5,6 +5,8 @@ import com.github.cloudyrock.mongock.ChangeSet
 import org.springframework.data.mongodb.core.MongoTemplate
 import ru.fmtk.khlystov.culture_code.model.Country
 import ru.fmtk.khlystov.culture_code.model.books.BookGenre
+import ru.fmtk.khlystov.culture_code.model.movies.MovieGenre
+import ru.fmtk.khlystov.culture_code.model.music.MusicGenre
 
 @ChangeLog(order = "001")
 class InitMongoDBDataChangeLog {
@@ -12,22 +14,26 @@ class InitMongoDBDataChangeLog {
     @ChangeSet(order = "000", id = "initBookGenres", author = "khlystov", runAlways = false)
     fun initBookGenres(template: MongoTemplate) {
         getListsOfBookGenres().forEach { (group, sequence) ->
-            sequence.forEach { genre -> template.save<BookGenre>(BookGenre(null, genre, group)) }
+            sequence.forEach { genre -> template.save(BookGenre(null, genre, group)) }
         }
     }
 
     @ChangeSet(order = "001", id = "initCountries", author = "khlystov", runAlways = false)
     fun initCountries(template: MongoTemplate) {
         getListOfCountries().forEach { (name, alpha3, iso) ->
-            template.save<Country>(Country(null, name, alpha3, iso.toShort()))
+            template.save(Country(null, name, alpha3, iso.toShort()))
         }
     }
 
     @ChangeSet(order = "002", id = "initMovieGenres", author = "khlystov", runAlways = false)
     fun initMovieGenres(template: MongoTemplate) {
-        getListsOfMovieGenres().forEach { genre -> template.save<BookGenre>(BookGenre(null, genre)) }
+        getListsOfMovieGenres().forEach { genre -> template.save(MovieGenre(null, genre)) }
     }
 
+    @ChangeSet(order = "003", id = "initMusicGenres", author = "khlystov", runAlways = false)
+    fun initMusicGenres(template: MongoTemplate) {
+        getListOfMusicGenres().forEach { genre -> template.save(MusicGenre(null, genre)) }
+    }
 
 //    @ChangeSet(order = "002", id = "initBooks", author = "khlystov", runAlways = false)
 //    fun initBooks(template: MongoTemplate) {
