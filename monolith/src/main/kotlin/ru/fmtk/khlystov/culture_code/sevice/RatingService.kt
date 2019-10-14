649a3acaf85c4a64d05e769d40d5432006b8bd11
MaxHlystov <maxvls@gmail.com>
2019-10-15 00:31:14 +0300

    Added rating service interface.

---
+++ monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/sevice/RatingService.kt
@@ -0,0 +1,10 @@
+package ru.fmtk.khlystov.culture_code.sevice
+
+import org.springframework.stereotype.Service
+import ru.fmtk.khlystov.culture_code.model.ratings.ItemType
+
+@Service
+interface RatingService {
+    fun setRating(userId: String, itemType: ItemType, itemId: String, rating: Short)
+    fun clearRating(userId: String, itemType: ItemType, itemId: String)
+}
