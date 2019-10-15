8ac2fcfbfb63c32312118e140766986ff3f30817
MaxHlystov <maxvls@gmail.com>
2019-10-16 02:44:14 +0300

    Added some code to computeRecommendations function

--- monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/sevice/RecommendationServiceImpl.kt
+++ monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/sevice/RecommendationServiceImpl.kt
@@ -3,14 +3,19 @@ package ru.fmtk.khlystov.culture_code.sevice
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
+import ru.fmtk.khlystov.culture_code.model.User
 import ru.fmtk.khlystov.culture_code.model.ratings.ItemType
 import ru.fmtk.khlystov.culture_code.model.recomendations.Recommendations
+import ru.fmtk.khlystov.culture_code.repository.UserRepository
+import ru.fmtk.khlystov.culture_code.repository.ratings.UserItemRatingRepository
 import ru.fmtk.khlystov.culture_code.repository.recommendations.RecommendationsRepository
 import ru.fmtk.khlystov.culture_code.sevice.dto.RecommendationsDTO
 
 @Service
 class RecommendationServiceImpl(
-        private val recommendationsRepository: RecommendationsRepository
+        private val recommendationsRepository: RecommendationsRepository,
+        private val userItemRatingRepository: UserItemRatingRepository,
+        private val userRepository: UserRepository
 ) : RecommendationService {
 
     override fun getRecommendations(userId: String, itemType: ItemType, number: Short): RecommendationsDTO {
@@ -21,6 +26,24 @@ class RecommendationServiceImpl(
     }
 
     override fun computeRecommendations() {
+        val sort = Sort.by("id").descending()
+        val users = userRepository.findAll(sort).toList()
+        val closeness = getUsersCloseness(users)
+        for(user in users) {
+            if(closeness.containsKey(user)) {
+                val neighbors = closeness[user]
 
+            } else {
+                // Search for items with the biggest average rating
+                for(itemType in ItemType.values()) {
+                    val avgRatings = userItemRatingRepository.getAVGRatingsForItemType(itemType)
+                }
+            }
+        }
+
+    }
+
+    private fun getUsersCloseness(users: List<User>, numberOfUsers: Int = 5): Map<User, List<User>> {
+        return HashMap()
     }
 }
