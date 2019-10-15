9e4922b0cde225efbd4c4305867fca90f0a4396c
MaxHlystov <maxvls@gmail.com>
2019-10-16 02:21:14 +0300

    Fix function getRecommendations

--- monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/sevice/RecommendationServiceImpl.kt
+++ monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/sevice/RecommendationServiceImpl.kt
@@ -1,17 +1,26 @@
 package ru.fmtk.khlystov.culture_code.sevice
 
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import ru.fmtk.khlystov.culture_code.model.ratings.ItemType
-import ru.fmtk.khlystov.culture_code.repository.recommendations.recommendationsRepository
+import ru.fmtk.khlystov.culture_code.model.recomendations.Recommendations
+import ru.fmtk.khlystov.culture_code.repository.recommendations.RecommendationsRepository
 import ru.fmtk.khlystov.culture_code.sevice.dto.RecommendationsDTO
 
 @Service
-class RecommendationServiceImpl(private val recommendationsRepository: recommendationsRepository) : RecommendationService {
+class RecommendationServiceImpl(
+        private val recommendationsRepository: RecommendationsRepository
+) : RecommendationService {
+
     override fun getRecommendations(userId: String, itemType: ItemType, number: Short): RecommendationsDTO {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val pageable = PageRequest.of(0, 5, Sort.by("rating").descending())
+        return RecommendationsDTO(userId, itemType,
+                recommendationsRepository.findAllByUserIdAndItemType(userId, itemType, pageable)
+                        .map(Recommendations::itemId).toSet())
     }
 
     override fun computeRecommendations() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
