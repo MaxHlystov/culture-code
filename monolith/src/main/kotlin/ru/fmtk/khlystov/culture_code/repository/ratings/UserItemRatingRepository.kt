a8635dd3990a759beef023dd77629d2e2e269e80
MaxHlystov <maxvls@gmail.com>
2019-10-16 02:20:45 +0300

    Renamed parameter and added new function getAVGRatings

--- monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/repository/ratings/UserItemRatingRepository.kt
+++ monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/repository/ratings/UserItemRatingRepository.kt
@@ -3,6 +3,7 @@ package ru.fmtk.khlystov.culture_code.repository.ratings
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import ru.fmtk.khlystov.culture_code.model.ratings.ItemAvgRating
 import ru.fmtk.khlystov.culture_code.model.ratings.ItemType
 import ru.fmtk.khlystov.culture_code.model.ratings.UserItemRating
 import java.util.*
@@ -10,9 +11,11 @@ import java.util.*
 @RepositoryRestResource(path = "UserItemRating")
 interface UserItemRatingRepository : MongoRepository<UserItemRating, String>, UserItemRatingRepositoryCustom {
     fun findAllByUserIdAndItemTypeAndRatingGreaterThan(
-            userId: String, itemType: ItemType, lowerRatingBound: Float, pageable: Pageable): Set<UserItemRating>
+            userId: String, itemType: ItemType, ratingGreaterThen: Float, pageable: Pageable): Set<UserItemRating>
 }
 
 interface UserItemRatingRepositoryCustom {
     fun save(userItemRating: UserItemRating): Optional<UserItemRating>
+
+    fun getAVGRatings(): List<ItemAvgRating>
 }
