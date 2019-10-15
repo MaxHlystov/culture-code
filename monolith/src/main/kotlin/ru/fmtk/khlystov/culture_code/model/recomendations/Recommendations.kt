b870f4a64efaf286540e32480579bb5c4c58c42e
MaxHlystov <maxvls@gmail.com>
2019-10-16 01:26:42 +0300

    Added compound index for userId, itemType, itemId fields

--- monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/model/recomendations/Recommendations.kt
+++ monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/model/recomendations/Recommendations.kt
@@ -1,12 +1,15 @@
 package ru.fmtk.khlystov.culture_code.model.recomendations
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.CompoundIndexes
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.format.annotation.DateTimeFormat
 import ru.fmtk.khlystov.culture_code.model.ratings.ItemType
 import java.time.LocalDateTime
 
 @Document(collection = "Recommendations_Recommendations")
+@CompoundIndexes(CompoundIndex(name = "itemTypeAndId", def = "{'userId' : 1, 'itemType' : 1, 'itemId': 1}"))
 data class Recommendations(@Id val id: String,
                            val userId: String,
                            val itemType: ItemType,
