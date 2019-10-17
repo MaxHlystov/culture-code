e102bbae586d4184943e444ad46348737283b7c8
MaxHlystov <DhwU2qXsZ7M81Pm3ws>
2019-10-17 15:36:50 +0300

    Fixed ratings query for exclude zero values, which means absence of rating.

--- monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/repository/ratings/UserItemRatingRepository.kt
+++ monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/repository/ratings/UserItemRatingRepository.kt
@@ -2,14 +2,19 @@ package ru.fmtk.khlystov.culture_code.repository.ratings
 
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.stereotype.Repository
 import ru.fmtk.khlystov.culture_code.model.ratings.ItemAvgRating
 import ru.fmtk.khlystov.culture_code.model.ratings.ItemType
 import ru.fmtk.khlystov.culture_code.model.ratings.UserItemRating
 import java.util.*
 
-@RepositoryRestResource(path = "UserItemRating")
+//@RepositoryRestResource(path = "UserItemRating")
+@Repository
 interface UserItemRatingRepository : MongoRepository<UserItemRating, String>, UserItemRatingRepositoryCustom {
+    @Query("{ 'itemType': ?0, 'rating': { \$gt: 0} }")
     fun findAllByItemType(itemType: ItemType): List<UserItemRating>
     fun findAllByUserIdAndItemTypeAndRatingGreaterThan(
             userId: String, itemType: ItemType, ratingGreaterThen: Float, pageable: Pageable): List<UserItemRating>
