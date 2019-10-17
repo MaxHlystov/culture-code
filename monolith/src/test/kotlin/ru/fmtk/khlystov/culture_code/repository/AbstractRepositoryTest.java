dd45132637a1a0bda8dbe26939c60e441464a351
MaxHlystov <DhwU2qXsZ7M81Pm3ws>
2019-10-17 13:38:51 +0300

    Added test for compute average ratings by items.

--- monolith/src/test/kotlin/ru/fmtk/khlystov/culture_code/repository/AbstractRepositoryTest.java
+++ monolith/src/test/kotlin/ru/fmtk/khlystov/culture_code/repository/AbstractRepositoryTest.java
@@ -3,11 +3,12 @@ package ru.fmtk.khlystov.culture_code.repository;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 
 @DataMongoTest
-@EnableConfigurationProperties
-@ComponentScan({"ru.fmtk.khlystov.culture_code.config", "ru.fmtk.khlystov.culture_code.repository"})
+//@EnableConfigurationProperties
+@ComponentScan({"ru.fmtk.khlystov.culture_code.config"}) //, "ru.fmtk.khlystov.culture_code.repository"})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-abstract class AbstractRepositoryTest {
+public abstract class AbstractRepositoryTest {
 }
--- monolith/src/test/kotlin/ru/fmtk/khlystov/culture_code/repository/ratings/UserItemRatingRepositoryImplTest.kt
+++ monolith/src/test/kotlin/ru/fmtk/khlystov/culture_code/repository/ratings/UserItemRatingRepositoryImplTest.kt
@@ -1,14 +1,19 @@
 package ru.fmtk.khlystov.culture_code.repository.ratings
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
 import ru.fmtk.khlystov.culture_code.model.ratings.ItemAvgRating
+import ru.fmtk.khlystov.culture_code.model.ratings.ItemType
 import ru.fmtk.khlystov.culture_code.model.ratings.UserItemRating
+import ru.fmtk.khlystov.culture_code.repository.AbstractRepositoryTest
 
+@Import(value = [UserItemRatingRepositoryImpl::class])
 @DisplayName("UserItemRatingRepository must")
-internal class UserItemRatingRepositoryImplTest {
+internal class UserItemRatingRepositoryTest : AbstractRepositoryTest() {
 
     @Autowired
     lateinit var userItemRatingRepository: UserItemRatingRepository
@@ -35,13 +40,13 @@ internal class UserItemRatingRepositoryImplTest {
     @DisplayName(" return average ratings by types of items.")
     @Test
     fun getAVGRatingsForItemType() {
-        val allRatings = userItemRatingRepository.findAll().filterNotNull()
+        val allRatings = userItemRatingRepository.findAllByItemType(ItemType.BOOK)
         val computedAvg = allRatings.groupBy { ratingItem -> ratingItem.itemType to ratingItem.itemId }
                 .map { (idPair, ratingItems) ->
                     ItemAvgRating(idPair.first, idPair.second,
                             ratingItems.map(UserItemRating::rating).average().toFloat())
                 }
-        val avg = userItemRatingRepository.getAVGRatingsForItemType()
+        val avg = userItemRatingRepository.getAVGRatingsForItemType(ItemType.BOOK)
         assertEquals(5, avg.size)
         assertTrue(avg.asSequence().zipWithNext()
                 .all { (first, second) -> first.avgRating >= second.avgRating },
