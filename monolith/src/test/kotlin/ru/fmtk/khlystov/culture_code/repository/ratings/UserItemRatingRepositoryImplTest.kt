2c3a3b9467f9b10d855c370878ddb0e299ac3e6b
MaxHlystov <DhwU2qXsZ7M81Pm3ws>
2019-10-17 12:34:02 +0300

    Added test for compute average ratings by items.

--- monolith/src/test/kotlin/ru/fmtk/khlystov/culture_code/repository/ratings/UserItemRatingRepositoryImplTest.kt
+++ monolith/src/test/kotlin/ru/fmtk/khlystov/culture_code/repository/ratings/UserItemRatingRepositoryImplTest.kt
@@ -1,18 +1,55 @@
 package ru.fmtk.khlystov.culture_code.repository.ratings
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import ru.fmtk.khlystov.culture_code.model.ratings.ItemAvgRating
+import ru.fmtk.khlystov.culture_code.model.ratings.UserItemRating
 
+@DisplayName("UserItemRatingRepository must")
 internal class UserItemRatingRepositoryImplTest {
 
-    @Test
-    fun save() {
-    }
+    @Autowired
+    lateinit var userItemRatingRepository: UserItemRatingRepository
 
+//    @Autowired
+//    lateinit var bookRepository: BookRepository
+//
+//    @Autowired
+//    lateinit var movieRepository: MovieRepository
+//
+//    lateinit var allBooks: Map<String, Book>
+//    lateinit var allMovies: Map<String, Movie>
+//
+//    @BeforeEach
+//    internal fun beforeEach() {
+//        allBooks = bookRepository.findAll()
+//                .filter { book -> book.id != null }
+//                .associate { book -> (book.id ?: "") to book }
+//        allMovies = movieRepository.findAll()
+//                .filter { movie -> movie.id != null }
+//                .associate { movie -> (movie.id ?: "") to movie }
+//    }
+
+    @DisplayName(" return average ratings by types of items.")
     @Test
     fun getAVGRatingsForItemType() {
+        val allRatings = userItemRatingRepository.findAll().filterNotNull()
+        val computedAvg = allRatings.groupBy { ratingItem -> ratingItem.itemType to ratingItem.itemId }
+                .map { (idPair, ratingItems) ->
+                    ItemAvgRating(idPair.first, idPair.second,
+                            ratingItems.map(UserItemRating::rating).average().toFloat())
+                }
+        val avg = userItemRatingRepository.getAVGRatingsForItemType()
+        assertEquals(5, avg.size)
+        assertTrue(avg.asSequence().zipWithNext()
+                .all { (first, second) -> first.avgRating >= second.avgRating },
+                "Ratings has to be ordered in decrease way.")
+        assertEquals(computedAvg, avg)
     }
 
+    @DisplayName(" return closeness of users.")
     @Test
     fun getClosenessByRating() {
     }
