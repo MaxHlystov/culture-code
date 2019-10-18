b67cfd140601d04fbe68a7da2d76bdad85d48b9e
MaxHlystov <DhwU2qXsZ7M81Pm3ws>
2019-10-18 19:22:22 +0300

    Added link to /recommendations rest-controller.

--- monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/rest/RecommendationController.kt
+++ monolith/src/main/kotlin/ru/fmtk/khlystov/culture_code/rest/RecommendationController.kt
@@ -1,14 +1,19 @@
 package ru.fmtk.khlystov.culture_code.rest
 
+import org.springframework.data.rest.webmvc.RepositoryLinksResource
+import org.springframework.data.rest.webmvc.RepositoryRestController
+import org.springframework.hateoas.ResourceProcessor
+import org.springframework.hateoas.mvc.ControllerLinkBuilder
 import org.springframework.web.bind.annotation.*
 import ru.fmtk.khlystov.culture_code.model.ratings.ItemType
 import ru.fmtk.khlystov.culture_code.sevice.RecommendationService
 import ru.fmtk.khlystov.culture_code.sevice.dto.RecommendationsDTO
 
-@RestController
+
+@RepositoryRestController
 @RequestMapping(value = "/recommendations")
-class RecommendationController(
-        private val recommendationService: RecommendationService) {
+class RecommendationController(private val recommendationService: RecommendationService)
+    : ResourceProcessor<RepositoryLinksResource> {
 
     @GetMapping(value = "/userid/{userId}/itemtype/{itemType}/limit/{limit}")
     fun getRecommendationsForUserAndItemType(@PathVariable userId: String,
@@ -32,4 +37,10 @@ class RecommendationController(
         recommendationService.computeRecommendations()
     }
 
+    override fun process(resource: RepositoryLinksResource): RepositoryLinksResource {
+        resource.add(
+                ControllerLinkBuilder.linkTo(RecommendationController::class.java)
+                        .withRel("recommendations"))
+        return resource;
+    }
 }
